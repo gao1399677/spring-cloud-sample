47ae3ec8c82f648314fff6bdc1b7d6bc50ab8867
gao1399677 <daigao1129>
2019-03-01 09:58:28 +0800

    initiate with spring cloud bus and rabbitamqp

--- department-service/src/main/java/pl/piomin/services/department/rabbitmq/MessageOutput.java
+++ department-service/src/main/java/pl/piomin/services/department/rabbitmq/MessageOutput.java
@@ -5,11 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableBinding(Source.class)

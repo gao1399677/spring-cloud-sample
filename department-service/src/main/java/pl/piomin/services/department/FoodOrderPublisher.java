00b675722b409fb372d24d6a2959f766d898e7b8
gao1399677 <daigao1129>
2019-03-01 08:47:01 +0800

    initiate with spring cloud bus and rabbitamqp

--- department-service/src/main/java/pl/piomin/services/department/FoodOrderPublisher.java
+++
@@ -1,7 +0,0 @@
-package pl.piomin.services.department;
-
-import org.springframework.cloud.stream.annotation.EnableBinding;
- 
-@EnableBinding(FoodOrderSource.class)
-public class FoodOrderPublisher {
-}

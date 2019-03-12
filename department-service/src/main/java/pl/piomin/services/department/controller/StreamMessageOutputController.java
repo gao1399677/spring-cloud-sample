110ddac226db60e9d51a2c42bfa39500adbde73d
gao1399677 <daigao1129>
2019-03-12 16:01:12 +0800

    Spring stream

--- department-service/src/main/java/pl/piomin/services/department/controller/StreamMessageOutputController.java
+++ department-service/src/main/java/pl/piomin/services/department/controller/StreamMessageOutputController.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.RequestBody;

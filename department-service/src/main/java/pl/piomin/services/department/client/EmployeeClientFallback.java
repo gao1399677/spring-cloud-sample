5ea8d0b208e3a6df804f093e45731ceef15cbb47
gao1399677 <daigao1129>
2019-03-12 15:41:34 +0800

    Spring stream

--- department-service/src/main/java/pl/piomin/services/department/client/EmployeeClientFallback.java
+++ department-service/src/main/java/pl/piomin/services/department/client/EmployeeClientFallback.java
@@ -1,14 +1,13 @@
 package pl.piomin.services.department.client;
 
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import pl.piomin.services.department.model.Employee;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class EmployeeClientFallback implements EmployeeClient{
+public class EmployeeClientFallback implements EmployeeClient {
     @Override
     public List<Employee> findByDepartment(Long departmentId) {
         return new ArrayList<>();
